f9d6f7344a1a5ec4c6ba6d0f3ebc0a47323e1454
Luke Miles <luke@lukemil.es>
2020-01-15 16:06:36 -0800

    Add tracking javascript

--- script.js
+++ script.js
@@ -30,3 +30,10 @@ var setDate = function() {
 };
 setDate();
 setInterval(setDate, 1000);
+
+window.counter = 'https://posting.goatcounter.com/count'
+var script = document.createElement('script');
+script.async = 1;
+script.src = '//gc.zgo.at/count.js';
+var ins = document.getElementsByTagName('script')[0];
+ins.parentNode.insertBefore(script, ins);
