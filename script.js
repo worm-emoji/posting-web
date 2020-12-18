f417400a108107b6095ae8edc8900b8d5f892a76
Luke Miles <luke@lukemil.es>
2020-12-19 00:57:59 +0100

    Add privacy page

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-var setDate = function() {
+var setDate = function () {
   var date = new Date();
   var el = document.getElementById("time");
   var formatted =
     "[" +
-    date.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit" }) +
+    date.toLocaleTimeString(undefined, {
+      hour: "2-digit",
+      minute: "2-digit",
+    }) +
     "]";
+
   if (el.innerHTML !== formatted) {
     el.innerHTML = formatted;
   }
 };
 setDate();
 setInterval(setDate, 1000);
-
-window.counter = 'https://posting.goatcounter.com/count'
-var script = document.createElement('script');
+window.counter = "https://posting.goatcounter.com/count";
+var script = document.createElement("script");
 script.async = 1;
-script.src = '//gc.zgo.at/count.js';
-var ins = document.getElementsByTagName('script')[0];
+script.src = "//gc.zgo.at/count.js";
+var ins = document.getElementsByTagName("script")[0];
 ins.parentNode.insertBefore(script, ins);
